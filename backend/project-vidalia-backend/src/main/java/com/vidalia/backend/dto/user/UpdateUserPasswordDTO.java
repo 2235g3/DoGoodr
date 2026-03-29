5875b5a8b20aca6584c3df48ea73920f52584dd3
Tsvetan Dyulgerov <td00783@surrey.ac.uk>
2026-03-29 16:45:11 +0300

    fix: change password validation to include at least one symbol

--- backend/project-vidalia-backend/src/main/java/com/vidalia/backend/dto/user/UpdateUserPasswordDTO.java
+++ backend/project-vidalia-backend/src/main/java/com/vidalia/backend/dto/user/UpdateUserPasswordDTO.java
@@ -13,7 +13,7 @@ public class UpdateUserPasswordDTO {
 
     @NotBlank(message = "Password is required")
     @Schema(description = "Password must contain at least 1 letter, 1 number, and be at least 8 characters long", example = "Password123")
-    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
-            message = "Password must be at least 8 characters long, contain at least 1 letter and 1 number")
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must be at least 8 characters long, contain at least 1 letter, 1 number, and 1 special character")
     protected String newPassword;
 }
