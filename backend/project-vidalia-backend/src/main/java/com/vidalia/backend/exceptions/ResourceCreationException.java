8b457b27fba5ea2878830bcdd5e8c0dabc5264cc
Dewar, Jacob (UG - Comp Sci & Elec Eng) <jd01781@surrey.ac.uk>
2026-03-31 07:59:36 +0000

    Revert "Merge branch 'fix/email-length' into 'main'"
    This reverts merge request !7

--- backend/project-vidalia-backend/src/main/java/com/vidalia/backend/exceptions/ResourceCreationException.java
+++
@@ -1,7 +0,0 @@
-package com.vidalia.backend.exceptions;
-
-public class ResourceCreationException extends RuntimeException {
-    public ResourceCreationException(String message) {
-        super(message);
-    }
-}
